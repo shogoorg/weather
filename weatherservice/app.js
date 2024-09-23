9bf8646a19caf3bc18c23c38c68c6f9641c81ce1
Shogo <shogoorg@gmail.com>
2024-09-23 09:12:16 +0900

    Update weatherservice

--- weatherservice/app.js
+++ weatherservice/app.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const app = express();
+//const { BigQuery } = require('@google-cloud/bigquery');
+//const bigquery = new BigQuery();
 
 app.get("/getweather", (req, res) => {
     const location = req.query.location;
     let temp, conditions;
 
     if (location == "Tokyo") {
-        temp = 10;
+        //const query = "SELECT TRUNC((5 / 9) * (temp - 32), 1) AS Celsius,date FROM bigquery-public-data.noaa_gsod.gsod2023 where stn = '476620' order by date desc limit 1";
+        temp = 10.0;
         conditions = "2023-12-31";
     } else if (location == "Tokyo Intl") {
+        //const query = "SELECT TRUNC((5 / 9) * (temp - 32), 1) AS Celsius,date FROM bigquery-public-data.noaa_gsod.gsod2023 where stn = '476710' order by date desc limit 1";
         temp = 10.7;
         conditions = "2023-12-31";
     } else if (location == "Tokyo Heliport") {
+        //const query = "SELECT TRUNC((5 / 9) * (temp - 32), 1) AS Celsius,date FROM bigquery-public-data.noaa_gsod.gsod2023 where stn = '476870' order by date desc limit 1";
         temp = 10.2;
         conditions = "2023-12-31";
     } else {
